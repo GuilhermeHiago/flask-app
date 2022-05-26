c7107fccea8c57291a31765fbb6aee6d3d3ab0b7
GuilhermeHiago <guiga.santos1711@gmail.com>
2022-05-26 15:47:22 -0300

    dois

--- hello.py
+++ hello.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    game_list = ['God of War', 'Valorant', 'GTA V']
+    return render_template('list.html', title="Games", items=game_list)
