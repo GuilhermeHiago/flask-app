6dad223385576795e6644cba3c9d6f7fdbe27a7a
unknown <guiga.santos1711@gmail.com>
2022-05-27 16:11:00 -0300

    finishing game registration post method

--- hello.py
+++ hello.py
@@ -1,27 +1,32 @@
-import string
-from turtle import title
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Game:
-    def __init__(self, title:string, category) -> None:
+    def __init__(self, title, category) -> None:
         self.title = title
         self.category = category
 
+game1 = Game('God of War', 'Hank and Slash')
+game2 = Game('Valorant', 'FPS')
+game3 = Game('GTA V', 'Action')
+game_list = [game1, game2, game3]
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    game1 = Game('God of War', 'Hank and Slash')
-    game2 = Game('Valorant', 'FPS')
-    game3 = Game('GTA V', 'Action')
-    game_list = [game1, game2, game3]
     return render_template('list.html', title="Games", items=game_list)
 
 @app.route('/gameregister')
 def game_register_form():
     return render_template('gameregisterform.html', title="New Game")
 
-@app.route('/gamecreate')
+@app.route('/gamecreate', methods=['POST',])
 def game_create():
-    pass
+    title = request.form('title')
+    category = request.form('category')
+    console = request.form('console')
+
+    game = Game(title, category)
+    game_list.append(game)
+    
+    return render_template('list.html', title="Games", items=game_list)
