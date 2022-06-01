0398af8262e940c4124a8592d2000469df3d0722
GuilhermeHiago <guiga.santos1711@gmail.com>
2022-06-01 15:41:43 -0300

    update redirect using url_for

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash, url_for
 
 class Game:
     def __init__(self, title, category) -> None:
@@ -14,16 +14,16 @@ app = Flask(__name__)
 app.secret_key = "flaskapp"
 
 @app.route("/")
-def hello_world():
+def index():
     return render_template('list.html', title="Games", items=game_list)
 
 @app.route('/gameregister')
-def game_register_form():
+def game_register():
     if session['current_user'] == 'admin':
         return render_template('gameregisterform.html', title="New Game")
 
     flash('Authentication Required')
-    return redirect('/login')
+    return redirect(url_for('login', next=url_for('game_register')))
 
 @app.route('/gamecreate', methods=['POST',])
 def game_create():
@@ -38,11 +38,12 @@ def game_create():
     game_list.append(game)
     
     #return render_template('list.html', title="Games", items=game_list)
-    return redirect("/")
+    return redirect(url_for("index"))
 
 @app.route('/login')
 def login():
-    return render_template('login.html')
+    next = request.args.get('next')
+    return render_template('login.html', next=next)
 
 @app.route('/authenticate', methods=['POST',])
 def authenticate():
@@ -50,16 +51,18 @@ def authenticate():
         session['current_user'] = request.form['user']
         flash(session['current_user'] + ' loged in')
 
-        return redirect('/')
+        next_page = request.form['next']
+        print(next_page)
+        return redirect(next_page)
     else:
         flash('Login Failed')
-        return redirect('/login')
+        return redirect(url_for("login"))
 
 @app.route('/logout')
 def logout():
     session['current_user'] = None
     flash('Logout Successful')
 
-    return redirect('/')
+    return redirect(url_for("index"))
 
 app.run(debug=True)
